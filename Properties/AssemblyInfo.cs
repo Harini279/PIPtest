682fe37ba86cfcef59dbf61a3b7087bade2f06d9
Harini279 <85232976+Harini279@users.noreply.github.com>
2021-06-08 11:23:36 +0530

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("devutest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("devutest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aca978a5-cd40-44fc-87f6-74406f24bf90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using devtest;
+
+namespace devutest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Atest obj = new Atest();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int actual = obj.add(5, 5);
+            int expected = 10;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
