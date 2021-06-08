a1fa050f598ae18f7b3dcee12802e1b7ae643ed5
Harini279 <85232976+Harini279@users.noreply.github.com>
2021-06-08 11:18:11 +0530

    Add files via upload

---
+++ devtest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devtest
+{
+   public class Atest
+    {
+        public int add(int a,int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
